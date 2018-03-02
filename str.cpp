6a14e20472371786c03fa5ef869c869ac8e27f64
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-03-02 18:00:00 +0530

    Create str.cpp

---
+++ str.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+int s1,s2,max=0,i;	
+char a[100],b[100];
+cout<<"Enter the first string:"<<endl;
+cin>>a;
+cout<<"Enter the second string: "<<endl;
+cin>>b;
+s1=strlen(a);
+s2=strlen(b);
+if(s1>s2)
+{
+max=s1;	
+}
+else	
+{
+max=s2;	
+}
+for(i=0;i<max;i++)	
+{
+if(a[i]==b[i])		
+{
+cout<<a[i];		
+}
+else		
+{	
+break;		
+}	
+}	
+return 0;
+}
