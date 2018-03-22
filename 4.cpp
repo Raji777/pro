7b3bbf8d96a7820b86c1434f4d5c91a8256fecbf
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-03-22 17:33:28 +0530

    4.cpp

---
+++ 4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char t[30];
+char k;
+int i;
+cout<<"enter t";
+cin>>t;
+for(i=0;t[i]!=NULL;i++)
+{
+if(t[i]==k)
+{
+cout<<"the position isd"<<i;
+}
+}
+return 0;
+}
